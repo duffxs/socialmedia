3be52d422270c1dd8df23b711a48ab9ea37641de
Trevor List <tlist@iastate.edu>
2024-12-09 19:29:41 -0600

    Your commit message

--- api/posts.js
+++ api/posts.js
@@ -2,17 +2,27 @@
 let posts = [];
 
 module.exports = async (req, res) => {
+  // Check if request method is GET
   if (req.method === 'GET') {
     res.status(200).json(posts);  // Return posts
-  } else if (req.method === 'POST') {
-    const { text } = req.body;
-    if (text) {
-      posts.push({ text });
-      res.status(200).send('Post added');
-    } else {
-      res.status(400).send('Text is required');
+  } 
+  // Handle POST request
+  else if (req.method === 'POST') {
+    // Parse the JSON body manually
+    try {
+      const { text } = req.body;
+      if (text) {
+        posts.push({ text });
+        res.status(200).send('Post added');
+      } else {
+        res.status(400).send('Text is required');
+      }
+    } catch (error) {
+      res.status(400).send('Invalid JSON');
     }
-  } else {
+  } 
+  // Method Not Allowed
+  else {
     res.status(405).send('Method Not Allowed');
   }
 };
